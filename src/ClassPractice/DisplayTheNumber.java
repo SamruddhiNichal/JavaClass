01eeb02fd4c62a4ad297660ab54bb94b886e17c7
SamruddhiNichal <samruddhinichal191@gmail.com>
2021-02-17 12:31:41 +0530

    program

---
+++ src/ClassPractice/DisplayTheNumber.java
@@ -0,0 +1,16 @@
+package ClassPractice;
+
+public class DisplayTheNumber {
+    public static void main(String[] args) {
+
+        int num;
+
+
+        for (num=1;num<=100;num++){
+
+
+
+            System.out.println(num);
+        }
+    }
+}
