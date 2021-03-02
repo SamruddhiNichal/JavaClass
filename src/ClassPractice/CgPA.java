553d9b6a15101471b15c77f59f4e695741b7267c
SamruddhiNichal <samruddhinichal191@gmail.com>
2021-03-02 09:21:22 +0530

    Practice

---
+++ src/ClassPractice/CgPA.java
@@ -0,0 +1,11 @@
+package ClassPractice;
+
+public class CgPA {
+    public static void main(String[] args) {
+        float subject1=90;
+        float subject2=60;
+        float subject3=40;
+        float cgpa=(subject1 +subject2 +subject3)/30;
+        System.out.println(cgpa);
+    }
+}
