ded5046fbb271dea4b4f79f0fc803e84cab8cc6a
SamruddhiNichal <samruddhinichal191@gmail.com>
2021-03-02 09:23:35 +0530

    Practice

---
+++ src/ClassPractice/StringLowercase.java
@@ -0,0 +1,22 @@
+package ClassPractice;
+
+import java.util.Locale;
+
+public class StringLowercase {
+    public static void main(String[] args) {
+        // String name = "Samruddhi Nichal";
+        // name.toLowerCase();
+        // System.out.println(name);
+        // String letter = "Dear <|name|,Thanks a lot!";
+        // letter = letter.replace("<|name|","Samruddhi");
+        //  System.out.println(letter);
+
+
+        String letter = "Dear Samruddhi,\n\tThis course is nice.\n\tThanks!";
+        System.out.println(letter);
+    }
+
+    }
+
+
+
