453fd7e50c3d6f7c52e14b608d42879e6be12661
SamruddhiNichal <samruddhinichal191@gmail.com>
2021-03-04 09:30:16 +0530

    Arrays

---
+++ src/ClassPractice/ArrayExpection.java
@@ -0,0 +1,16 @@
+package ClassPractice;
+
+public class ArrayExpection {
+    public static void main(String[] args) {
+        int arr[] = new int[4];
+        arr[0] = 1;
+        arr[1] = 13;
+        arr[2] = 15;
+        arr[3] = 16;
+        arr[4] = 20;
+
+        for (int i = 0; i <= arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
